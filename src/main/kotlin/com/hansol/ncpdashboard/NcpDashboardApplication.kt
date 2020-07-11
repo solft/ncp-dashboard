a7f55fba3d820e05fd844fc6640e7eaf520ebd02
hansol <onesorry49@gmail.com>
2020-07-11 20:38:53 +0900

    first commit

---
+++ src/main/kotlin/com/hansol/ncpdashboard/NcpDashboardApplication.kt
@@ -0,0 +1,11 @@
+package com.hansol.ncpdashboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NcpDashboardApplication
+
+fun main(args: Array<String>) {
+    runApplication<NcpDashboardApplication>(*args)
+}
---
+++ src/test/kotlin/com/hansol/ncpdashboard/NcpDashboardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hansol.ncpdashboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NcpDashboardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
